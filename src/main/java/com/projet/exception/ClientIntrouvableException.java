5484d21ee2d58c260f338dfe506112afe83d5863
Romeo Lyams <romeolyams383@gmail.com>
2026-09-08 11:03:54 +0100

    Create ClientIntrouvableException

---
+++ src/main/java/com/projet/exception/ClientIntrouvableException.java
@@ -0,0 +1,8 @@
+package com.projet.exception;
+
+public class ClientIntrouvableException extends Exception {
+
+    public ClientIntrouvableException(String message) {
+        super(message);
+    }
+}
